df9c7df0464dc4b3511a589c0e348deabf9d411e
ALEX PARFENJUK <ololoa3272@gmail.com>
2015-07-07 22:35:41 +0300

    Something from basic logic

--- model/src/main/java/com/roodie/model/controllers/AboutItem.java
+++ model/src/main/java/com/roodie/model/controllers/AboutItem.java
@@ -4,4 +4,5 @@ package com.roodie.model.controllers;
  * Created by Roodie on 04.07.2015.
  */
 public enum AboutItem {
+
 }
---
+++ model/src/main/java/com/roodie/model/state/BaseState.java
@@ -0,0 +1,85 @@
+package com.roodie.model.state;
+
+import com.roodie.model.controllers.DrawerMenuItem;
+import com.roodie.model.network.NetworkError;
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+
+import java.util.List;
+
+/**
+ * Created by Roodie on 24.06.2015.
+ */
+public interface BaseState {
+
+    public void setSelectedSideMenuItem(DrawerMenuItem item);
+
+    public DrawerMenuItem getSelectedSideMenuItem();
+
+    public void registerForEvents(Object receiver);
+
+    public void unregisterForEvents(Object receiver);
+
+    static class BaseArgumentEvent<T> {
+        public final int callingId;
+        public final T item;
+
+        public BaseArgumentEvent(int callingId, T item) {
+            this.callingId = callingId;
+            this.item = Preconditions.checkNotNull(item, "item cannot be null");
+        }
+    }
+
+    public abstract static class PaginatedResult<T> {
+        public List<T> items;
+        public int page;
+        public int totalPages;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            PaginatedResult that = (PaginatedResult) o;
+            return Objects.equal(items, that.items);
+        }
+
+        @Override
+        public int hashCode() {
+            return items != null ? items.hashCode() : 0;
+        }
+    }
+
+    public static class ShowLoadingProgressEvent {
+        public final int callingId;
+        public final boolean show;
+        public final boolean secondary;
+
+        public ShowLoadingProgressEvent(int callingId, boolean show) {
+            this(callingId, show, false);
+        }
+
+        public ShowLoadingProgressEvent(int callingId, boolean show, boolean secondary) {
+            this.callingId = callingId;
+            this.show = show;
+            this.secondary = secondary;
+        }
+    }
+
+
+
+
+    public static class OnErrorEvent {
+        public final int callingId;
+        public final NetworkError error;
+
+        public OnErrorEvent(int callingId, NetworkError error) {
+            this.callingId = callingId;
+            this.error = error;
+        }
+    }
+
+}
