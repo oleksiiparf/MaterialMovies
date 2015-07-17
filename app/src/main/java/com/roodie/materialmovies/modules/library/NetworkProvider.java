ee0879925455b15f9ce0a1cbceb1263a44d72dd3
ALEX PARFENJUK <ololoa3272@gmail.com>
2015-07-17 23:07:46 +0300

    Add new Tmdb client to module.

--- app/src/main/java/com/roodie/materialmovies/modules/library/NetworkProvider.java
+++ app/src/main/java/com/roodie/materialmovies/modules/library/NetworkProvider.java
@@ -1,7 +1,14 @@
 package com.roodie.materialmovies.modules.library;
 
+import android.content.Context;
+
+import com.roodie.materialmovies.qualifiers.AppContext;
+import com.roodie.materialmovies.qualifiers.CacheDirectory;
+import com.roodie.materialmovies.util.MMoviesTmdb;
 import com.roodie.model.Constants;
 import com.uwetrottmann.tmdb.Tmdb;
+
+import java.io.File;
 
 import javax.inject.Singleton;
 
@@ -19,11 +26,20 @@ import dagger.Provides;
 
 public class NetworkProvider {
 
-    @Provides @Singleton
-    public Tmdb provideTmdbClient() {
-        Tmdb tmdb = new Tmdb();
+   @Provides @Singleton
+    public Tmdb provideTmdbClient(@AppContext Context context) {
+        Tmdb tmdb = new MMoviesTmdb(context);
         tmdb.setApiKey(Constants.TMDB_API_KEY);
         tmdb.setIsDebug(Constants.DEBUG_NETWORK);
         return tmdb;
     }
+
+
+    @Provides @Singleton @CacheDirectory
+    public File provideHttpCacheLocation(@AppContext Context context) {
+        return context.getCacheDir();
+    }
+
+
+
 }
