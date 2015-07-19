ad9b9254133f4214a9f7446f8e7a7c17c2a7032e
ALEX PARFENJUK <ololoa3272@gmail.com>
2015-07-19 16:35:53 +0300

    Using card to show detail information.

--- app/src/main/java/com/roodie/materialmovies/views/fragments/MovieDetailFragment.java
+++ app/src/main/java/com/roodie/materialmovies/views/fragments/MovieDetailFragment.java
@@ -12,12 +12,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.common.base.Preconditions;
 import com.roodie.materialmovies.R;
 import com.roodie.materialmovies.mvp.presenters.MovieDetailPresenter;
 import com.roodie.materialmovies.views.MMoviesApplication;
 import com.roodie.materialmovies.views.fragments.base.BaseDetailFragment;
+import com.roodie.model.entities.MovieCreditWrapper;
 import com.roodie.model.entities.MovieWrapper;
 import com.roodie.model.entities.PersonCreditWrapper;
 import com.roodie.model.network.NetworkError;
@@ -176,14 +179,21 @@ public class MovieDetailFragment extends BaseDetailFragment implements MovieDeta
         return new DetailAdapter();
     }
 
+    private void populateUi() {
+        if (mMovie == null) {
+            return;
+        }
+        mItems.clear();
+    }
+
     private enum DetailItemType implements DetailType {
-        TITLE(R.layout.item_movie_detail_title),
-        DETAILS(R.layout.item_movie_detail_details),
-        SUMMARY(R.layout.item_movie_detail_summary),
-        TRAILERS(R.layout.item_movie_detail_trailers),
-        RELATED(R.layout.item_movie_detail_generic_card),
-        CAST(R.layout.item_movie_detail_generic_card),
-        CREW(R.layout.item_movie_detail_generic_card);
+        TITLE(R.layout.item_movie_detail_title), //includes poster, tagline and rating
+        DETAILS(R.layout.item_movie_detail_details),//include details
+        SUMMARY(R.layout.item_movie_detail_summary),//includes description text, maybe
+        TRAILERS(R.layout.item_movie_detail_trailers),// includes trailers
+        RELATED(R.layout.item_movie_detail_generic_card),// includes related
+        CAST(R.layout.item_movie_detail_generic_card), /// includes cast list
+        CREW(R.layout.item_movie_detail_generic_card); // includes crew list
 
         private final int mLayoutId;
 
@@ -207,6 +217,62 @@ public class MovieDetailFragment extends BaseDetailFragment implements MovieDeta
             return false;
         }
     }
+
+
+    private abstract class BaseMovieCastAdapter extends BaseAdapter {
+        private final LayoutInflater mInflater;
+        private final View.OnClickListener mItemOnClickListener;
+
+        public BaseMovieCastAdapter(LayoutInflater mInflater, View.OnClickListener mItemOnClickListener) {
+            this.mInflater = mInflater;
+            this.mItemOnClickListener = mItemOnClickListener;
+        }
+
+        @Override
+        public abstract MovieCreditWrapper getItem(int position) ;
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(getLayoutId(), parent, false);
+            }
+
+            MovieCreditWrapper credit = getItem(position);
+
+            final TextView title = (TextView) convertView.findViewById(R.id.title);
+            title.setText(credit.getPerson().getName());
+
+            final ImageView image = (ImageView) convertView.findViewById(R.id.poster);
+            //Load with Picasso
+
+            final TextView subTitle = (TextView) convertView.findViewById(R.id.subtitle_1);
+            if (subTitle != null) {
+                if (!TextUtils.isEmpty(credit.getJob())) {
+                    subTitle.setText(credit.getJob());
+                    subTitle.setVisibility(View.VISIBLE);
+                } else {
+                    subTitle.setVisibility(View.GONE);
+                }
+            }
+
+            convertView.setOnClickListener(mItemOnClickListener);
+            convertView.setTag(credit);
+
+            return convertView;
+        }
+
+        protected int getLayoutId() {
+            return R.layout.item_movie_detail_list_2line;
+        }
+    }
+
+
 
 
     private class DetailAdapter extends BaseAdapter {
