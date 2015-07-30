0e96dcb5466cbae230fa449204e915e26f8c7ddc
ALEX PARFENJUK <ololoa3272@gmail.com>
2015-07-30 18:26:25 +0300

    Initialize MovieImagesPresenter

--- app/src/main/java/com/roodie/materialmovies/mvp/presenters/MovieImagesPresenter.java
+++ app/src/main/java/com/roodie/materialmovies/mvp/presenters/MovieImagesPresenter.java
@@ -1,18 +1,37 @@
 package com.roodie.materialmovies.mvp.presenters;
 
+import com.google.common.base.Preconditions;
 import com.roodie.materialmovies.mvp.views.MovieView;
-import com.uwetrottmann.tmdb.entities.Movie;
+import com.roodie.materialmovies.qualifiers.GeneralPurpose;
+import com.roodie.model.entities.MovieWrapper;
+import com.roodie.model.state.ApplicationState;
+import com.roodie.model.util.BackgroundExecutor;
+import com.roodie.model.util.Injector;
 
 import java.util.List;
+
+import javax.inject.Inject;
 
 /**
  * Created by Roodie on 06.07.2015.
  */
 public class MovieImagesPresenter extends BasePresenter {
 
-    private MovieImagesView mMoviesImagesView;
+    private MovieImagesView mView;
 
-    public MovieImagesPresenter() {
+    private final ApplicationState mState;
+    private final BackgroundExecutor mExecutor;
+    private final Injector mInjector;
+
+    private boolean attached = false;
+
+    @Inject
+    public MovieImagesPresenter(ApplicationState applicationState,
+                                @GeneralPurpose BackgroundExecutor executor,
+                                Injector injector) {
+        mState = Preconditions.checkNotNull(applicationState, "mState can not be null");
+        mExecutor = Preconditions.checkNotNull(executor, "executor cannot be null");
+        mInjector = Preconditions.checkNotNull(injector, "injector cannot be null");
     }
 
     @Override
@@ -21,20 +40,30 @@ public class MovieImagesPresenter extends BasePresenter {
     }
 
     @Override
-    protected void onInited() {
-        super.onInited();
+    public void initialize() {
+
+        checkViewAlreadySetted();
     }
 
     @Override
-    protected void onPaused() {
-        super.onPaused();
+    public void onPause() {
+
     }
 
+    public void attachView (MovieImagesView view) {
+        Preconditions.checkNotNull(view, "View cannot be null");
+        this.mView = view;
+        attached = true;
+
+    }
+
+    private void checkViewAlreadySetted() {
+        Preconditions.checkState(attached = true, "View not attached");
+    }
+
+
     public interface MovieImagesView extends MovieView {
-
-        void setItems(List<String> images);
-
-        void showMovieImages(Movie movie);
+        void setItems(List<MovieWrapper.BackgroundImage> images);
     }
 
 }
